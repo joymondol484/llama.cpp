99391dc923c33a80ddbab1093ebaae4382773c72
joymondol484 <joymondol2055@gmail.com>
2026-03-15 14:23:10 +0600

    Update build.gradle.kts

--- examples/llama.android/lib/build.gradle.kts
+++ examples/llama.android/lib/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.arm.aichat"
-    compileSdk = 26
+    compileSdk = 36
 
     ndkVersion = "29.0.13113456"
 
     defaultConfig {
-        minSdk = 33
+        minSdk = 26
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
